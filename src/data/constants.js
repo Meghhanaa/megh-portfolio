f76da24940044cc86da56af91c300f4829a28226
Meghana Tamrakar <tamrakarmeghana@gmail.com>
2025-01-12 23:52:32 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,348 @@
+export const Bio = {
+    name: "Meghana Tamrakar",
+    roles: [
+      "Full Stack Developer",
+      "Programmer",
+      "Data Scientist"
+    ],
+    description: "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+    github: "https://github.com/Meghhanaa",
+    resume: "https://drive.google.com/file/d/1_hX5YxO1eEPyuymO3eetWrCacSQry5I4/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/meghana-tamrakar-64804521b/",
+    // twitter: "https://twitter.com/kjcreddy666",
+    insta: "https://www.instagram.com/itzz_meghana_tamrakar?igsh=dmZrMHBlMWVva3h0",
+    // facebook: "https://www.facebook.com/kjcreddy666/",
+};
+  
+export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "C",
+          image: "C-logo.png",
+        },
+        {
+          name: "C++",
+          image: "CPP-logo.png",
+        },
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        },
+        {
+          name: "Python",
+          image: "Python-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Full-Stack",
+      skills: [
+        {
+          name: "HTML",
+          image: "html5-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "CSS3-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "bootstrap-logo.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "JavaScript-logo.png",
+        },
+        {
+          name: "Node Js",
+          image: "nodejs-logo.svg",
+        },
+        {
+          name: "React Js",
+          image: "react.jpeg",
+        },
+        // {
+          // name: "React Js",
+          // image:
+          //   // "react.",
+        // },
+      ],
+    },
+    {
+      title: "Data Science & ML",
+      skills: [
+        {
+          name: "Data Cleaning",
+          image: "data-cleaning.jpg",
+        },
+        {
+          name: "Data Visualization",
+          image: "data-vis.jpeg",
+        },
+        {
+          name: "Statistical Analysis",
+          image: "analytics.jpeg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "GitHub-logo.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "vscode-logo.png",
+        },
+      ],
+    },
+];
+  
+export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Docker",
+        "Terraform",
+        "AWS",
+        "EC2",
+        "Portainer",
+        "Nginx",
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+      role: "Frontend Engineer Intern",
+      company: "Neurobit",
+      date: "June 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "Fullstack Externship",
+      company: "Flipr",
+      date: "June 2023 - July 2023",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "AWS",
+        "MongoDB",
+      ],
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source Contributor ",
+      company: "GirlScript Summer of Code",
+      date: "May 2023 - Present",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+      doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+      role: "Android Developer",
+      company: "DSC KIIT",
+      date: "Nov2021 - Present",
+      desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+      skills: [
+        "Leadership",
+        "Mobile Application Development",
+        "Kotlin",
+        "XML",
+        "Figma",
+      ],
+    },
+    {
+      id: 4,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+      role: "Android Developer Intern",
+      company: "Rudraksha Welffare Foundation",
+      date: "June 2021 - Oct 2021",
+      desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+      skills: [
+        "Android",
+        "Java",
+        "Kotlin",
+        "XML",
+        "Node Js",
+        "Cloud Firestore",
+        "Firebase",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+    },
+];
+  
+export const education = [
+    {
+      id: 0,
+      img: "manit.png",
+      school: "Maulana Azad National Institute of Technology, Bhopal",
+      date: "2023 - 2026",
+      grade: "9.0 CGPA",
+      desc: " During my time at MANIT, I focused on advanced topics in computer science, including algorithms, software development, and database management. I engaged in various projects and internships that enhanced my practical skills and theoretical knowledge. My coursework included subjects such as Data Structures, Operating Systems, Web Technologies, and Artificial Intelligence. I also actively participated in coding competitions and tech fests, showcasing my ability to tackle complex problems and develop innovative solutions.",
+      degree: "Master's in Computer Application",
+    },
+    {
+      id: 1,
+      img: "apsu.jpeg",
+      school: "Awdhesh Pratap Singh University, Rewa, MP",
+      date: "2020 - 2023",
+      grade: "8.9 GPA",
+      desc: "I laid the foundation for my career in computer science. I gained a solid understanding of programming languages, software development methodologies, and computer networks. My curriculum covered essential subjects such as Object-Oriented Programming, Computer Architecture, and Database Management Systems. Additionally, I undertook various projects that honed my coding skills and prepared me for real-world challenges. I was also an active member of the computer science club, where I collaborated with peers on tech projects and organized workshops.",
+      degree: "Bachelor's in Computer Application",
+    },
+    {
+      id: 2,
+      img: "gvs.jpeg",
+      school: "Gyanasthali Vidyalaya, Rewa",
+      date: "2020",
+      grade: "86%",
+      desc: "I completed my class 12 education atGyanasthali Vidyalaya, Rewa",
+      degree: "Central Board Of Secondary Education",
+    },
+];
+  
+export const projects = [
+    {
+      id: 9,
+      title: "E-Visitors Hub",
+      description:
+        "Designed an interactive platform to provide up-to-date information on attractions, accommodations, and emergency services which Enhanced user engagement by 25% and increased average session duration by 40% through intuitive design and features.",
+      image:
+        "evisit.jpeg",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "Bootstrap",
+        "PHP",
+        "MySQL"
+      ],
+      category: "web app",
+      github: "https://github.com/Meghhanaa/E-visitors",
+      webapp: "",
+    },
+
+    {
+      id: 9,
+      title: "EzyRide – Vehicle Renting Management System",
+      description:
+        "Developing a robust vehicle management system to streamline rental operations. Impact: Expected to reduce rental processing time by 40% through real-time data tracking, user management, and vehicle registration functionalities.",
+      image:
+        "ezyride.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React",
+        "Node",
+        "Mysql"
+      ],
+      category: "web app",
+      github: "https://github.com/Meghhanaa/EZYRIDE",
+      webapp: "#",
+    },
+
+    // {
+    //   // id: 9,
+    //   // title: "",
+    //   // description:
+    //   //   "Implemented a classic Tic Tac Toe game using HTML5, CSS3, JavaScript and ReactJS.",
+    //   // image:
+    //   //   "tictactoe-logo.png",
+    //   // tags: [
+    //   //   "HTML5",
+    //   //   "CSS3",
+    //   //   "JavaScript",
+    //   //   "ReactJs"
+    //   // ],
+    //   // category: "web app",
+    //   // github: "https://github.com/kjcreddy666/tictactoe",
+    //   // webapp: "https://kjcreddy666.github.io/tictactoe",
+    // },
+
+    // {
+    //   id: 9,
+    //   title: "Simon-says",
+    //   description:
+    //     `"Simon Says" is an exciting web project designed to keep users hooked on a memory challenge.`,
+    //   image:
+    //     "simon-logo.png",
+    //   tags: [
+    //     "HTML5",
+    //     "CSS3",
+    //     "JavaScript",
+    //   ],
+    //   category: "web app",
+    //   github: "https://github.com/kjcreddy666/simon",
+    //   webapp: "https://kjcreddy666.github.io/portfolio",
+    // },
+
+    {
+      id: 9,
+      title: "Netflix-clone",
+      description:
+        "This project is a Netflix clone, aiming to replicate the user interface of the popular music streaming service using React js.The primary goal of this project is to practice front-end as well as the backend development skill",
+      image:
+        "netflix.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "Bootstrap",
+        "React js",
+        "Firebase"
+      ],
+      category: "web app",
+      github: "https://github.com/Meghhanaa/Netflix-clone",
+      webapp: "#",
+    },
+];
+  
+export const TimeLineData = [
+    // { year: 2017, text: "Started my journey" },
+    // { year: 2018, text: "Worked as a freelance developer" },
+    // { year: 2019, text: "Founded JavaScript Mastery" },
+    // { year: 2020, text: "Shared my projects with the world" },
+    // { year: 2021, text: "Started my own platform" },
+];
